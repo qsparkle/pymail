8e42b508ccf4e88f434f2e98c31dcb2907c584cf
qsparkle <89706068+qsparkle@users.noreply.github.com>
2021-09-04 12:07:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import os
 import smtplib
 from email.message import EmailMessage
 
-email_account = 'YOUR EMAIL'
-recipient_account = "RECEIVER'S EMAIL"
-email_pass = 'APP Password'
+email_account = 'justindzisah@gmail.com'
+recipient_account = "officialjdtech@gmail.com"
+email_pass = 'ibazoxjtstchildf'
 
-msg = EmailMessage()
+msg = EmailMessage(Hello is this spam)
 msg['From']=email_account
 msg['To']=recipient_account
 msg['subject']='Python Automation'
 msg.set_content('''
-YOU EMAIL GOES HEAR
+IS THIS SPAM
 ''')
 
 
